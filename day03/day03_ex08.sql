00a6e35c0f8ddd3ef4a4df95d3b90d34f357a3f0
zotov88 <zotov_l88@mail.ru>
2023-07-25 16:21:29 +0300

    day03

---
+++ day03/day03_ex08.sql
@@ -0,0 +1,5 @@
+insert into menu(id, pizzeria_id, pizza_name, price)
+values ((select max(id) + 1 from menu),
+        (select id from pizzeria where name = 'Dominos'),
+        'sicilian pizza',
+        900);
