8d4682f058cbbe90fac72636b9d17ecec346987c
zotov88 <zotov_l88@mail.ru>
2023-07-26 17:23:19 +0300

    day04

---
+++ day05/day05_ex02.sql
@@ -0,0 +1,10 @@
+create index idx_person_name on person (upper(name));
+
+set enable_seqscan = off;
+
+explain analyze
+select *
+from person
+where upper(name) = 'ANNA';
+
+set enable_seqscan = on;
