838efd4a2ff9df974397dbf7a47175210df4df45
zotov88 <zotov_l88@mail.ru>
2023-07-28 11:26:02 +0300

    day08

---
+++ day08/day08_ex03.sql
@@ -0,0 +1,20 @@
+-- session 1
+begin transaction isolation level read committed;
+
+-- session 2
+begin transaction isolation level read committed;
+
+-- session 1
+select * from pizzeria where name = 'Pizza Hut';
+
+-- session 2
+UPDATE pizzeria set rating = 3.6 where name = 'Pizza Hut';
+commit;
+
+-- session 1
+select * from pizzeria where name = 'Pizza Hut';
+commit;
+select * from pizzeria where name = 'Pizza Hut';
+
+-- session 2
+select * from pizzeria where name = 'Pizza Hut';
