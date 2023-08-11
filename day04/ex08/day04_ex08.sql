eb273513009453c23cd89d4178b3ecfb55f5caea
zotov88 <zotov_l88@mail.ru>
2023-08-11 15:56:32 +0300

    change the structure

--- day04/ex08/day04_ex08.sql
+++ day04/ex08/day04_ex08.sql
@@ -1,6 +1,6 @@
-drop materialized view mv_dmitriy_visits_and_eats;
-drop view v_generated_dates;
-drop view v_persons_female;
-drop view v_persons_male;
-drop view v_price_with_discount;
-drop view v_symmetric_union;
+drop materialized view if exists mv_dmitriy_visits_and_eats;
+drop view if exists v_generated_dates;
+drop view if exists v_persons_female;
+drop view if exists v_persons_male;
+drop view if exists v_price_with_discount;
+drop view if exists v_symmetric_union;
