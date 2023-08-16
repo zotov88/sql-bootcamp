1777d3f644e288e6b0ca643dcbc688cce6946dc2
zotov88 <zotov_l88@mail.ru>
2023-08-16 12:21:54 +0300

    day07

--- day07/ex09/day07_ex09.sql
+++ day07/ex09/day07_ex09.sql
@@ -1,7 +1,7 @@
 select address,
        round(max(age) - min(age)::numeric / max(age), 2)  as formula,
        round(avg(age), 2)                                 as average,
-       max(age) - min(age)::numeric / max(age) > avg(age) as comparison
+       (max(age) - min(age)::numeric / max(age)) > avg(age) as comparison
 from person
 group by address
 order by 1;
