f3f2478bd4778ce665b0ba48e42d2caad91f8955
zotov88 <zotov_l88@mail.ru>
2023-08-20 16:40:09 +0300

    day06 ex02 исправлено

--- day06/ex02/day06_ex02.sql
+++ day06/ex02/day06_ex02.sql
@@ -4,9 +4,9 @@ select p.name,
        (price - price * discount / 100) as discount_price,
        p2.name                          as pizzeria_name
 from person_order
-         join public.menu m on m.id = person_order.menu_id
-         join public.person p on p.id = person_order.person_id
-         join public.person_discounts pd on p.id = pd.person_id
+         join menu m on m.id = person_order.menu_id
+         join person p on p.id = person_order.person_id
+         join person_discounts pd on p.id = pd.person_id
     and pd.pizzeria_id = m.pizzeria_id
-         join public.pizzeria p2 on m.pizzeria_id = p2.id
+         join pizzeria p2 on m.pizzeria_id = p2.id
 order by 1, 2;
