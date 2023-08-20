122eb468347cb5f2b81e3ad22b23eb1dd162b0d7
zotov88 <zotov_l88@mail.ru>
2023-08-20 22:41:02 +0300

    day09 ex08 исправлено

--- day09/ex08/day09_ex08.sql
+++ day09/ex08/day09_ex08.sql
@@ -1,21 +1,20 @@
 drop function if exists fnc_fibonacci(pstop integer);
 
 create or replace function fnc_fibonacci(in pstop integer default 10)
-    returns setof decimal as
+    returns setof integer as
 $body$
-with recursive fib(id, current, previous) as (select 1, 0::decimal, 0::decimal
+with recursive fib(id, current, previous) as (select 1, 0, 0
                                               union all
-                                              select 2, 1::decimal, 0::decimal
+                                              select 2, 1, 0
                                               union all
                                               select id + 1,
-                                                     (current + previous)::decimal,
-                                                     current::decimal
+                                                     current + previous,
+                                                     current
                                               from fib
-                                              where id < pstop
+                                              where current + previous < pstop
                                                 and id > 1)
 select current
 from fib
-limit pstop;
 $body$ language sql;
 
 select *
